44d63cc35a7fb724f61327d30be90d18658ecc17
mac <flamekung123146@gmail.com>
2024-07-13 17:29:45 +0700

    war crime

---
+++ src/main.blog.js
@@ -0,0 +1,33 @@
+import tippy from "tippy.js"
+
+import metadata from "/metadata.json"
+
+function renderBlogPosts() {
+  const container = document.getElementById("blog-posts")
+  if (!container) {
+    console.error(`Container element with id 'blog-posts' not found.`)
+    return
+  }
+
+  Object.keys(metadata).forEach((key) => {
+    const post = metadata[key]
+    const postElement = document.createElement("div")
+    postElement.classList.add("blog-post")
+    postElement.innerHTML += `
+    <div class="posts mt-2 mb-2 border-l-2 pl-1.5 border-rose-700">
+    <a href="/blog/${key}">
+      <h1>${post.author} - ${post.title}</h1>
+      <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
+      <p>Tags: ${post.tags.join(", ")}</p>
+</a>
+</div>
+    `
+    container.appendChild(postElement)
+  })
+}
+
+tippy(".posts", {
+  content: "My tooltip!",
+})
+
+document.addEventListener("DOMContentLoaded", renderBlogPosts)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,html}", "./blog/**/*.html"],
+  content: [
+    "./index.html",
+    "./blog.html",
+    "./src/**/*.{js,html}",
+    "./blog/**/*.html",
+  ],
   theme: {
     extend: {},
   },
